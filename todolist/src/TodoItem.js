402d53093963c6d4f1ecb38d444959a30e07fd38
yan-er <973583680@qq.com>
2019-04-02 11:14:24 +0800

    commit newtodolist

--- todolist/src/TodoItem.js
+++ todolist/src/TodoItem.js
@@ -1,16 +1,45 @@
 import React, { Component} from 'react';
+import PropTypes from 'prop-types';
+
 class TodoItem extends Component{
     constructor(props){
         super(props);
         this.handleClick = this.handleClick.bind(this);
     }
+    shouldComponentUpdate(nextProps,nextState){
+        if(nextProps.content !== this.props.content ){
+            return true;
+        }else{
+            return false;
+        }
+    }
     render(){
-        return <div onClick= {this.handleClick}>{
-            this.props.content}
+        const { content,test }=this.props;
+        return <div onClick= {this.handleClick}>
+        {test} - {content}
             </div>
     }
     handleClick(){
-        this.props.deleteItem(this.props.index);
+        const { deleteItem,index }=this.props;
+        deleteItem(index);
+    }
+
+    //一个组件要从父组件接收参数
+    // 只要父组件的render函数被重新执行了，子组件的这个生命周期函数就会被执行。
+    //如果这个组件第一次存在于父组件中，不会执行
+    //如果这个组件之前已经存在于父组件中，才会执行。
+    componentWillReceiveProps(){
+        console.log('child componentWillReceiveProps');
     }
 }
+TodoItem.propTypes={
+     test:PropTypes.string.isRequired,   
+    content: PropTypes.string,
+    deleteItem: PropTypes.func,
+    index: PropTypes.number
+}
+TodoItem.defaultProps={
+    test:'hello world'
+}
+
 export default TodoItem;
--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,6 +1,7 @@
 
 import React, { Component, Fragment } from 'react';
 import TodoItem from './TodoItem';
+//import axios from 'axios';
 class TodoList extends Component{
     constructor(props){
         super(props);
@@ -8,7 +9,12 @@ class TodoList extends Component{
             inputValue: '',
             list :[]
         }
+        this.handleInputChange=this.handleInputChange.bind(this);
+        this.handleBtnClick=this.handleBtnClick.bind(this);
+        this.handleItemDelete=this.handleItemDelete.bind(this);
     }
+    
+    
     render(){
         return (
             <Fragment>
@@ -19,55 +25,94 @@ class TodoList extends Component{
                         id="insertArea"
                         className='input'
                         value={ this.state.inputValue }
-                        onChange = {this.handleInputChange.bind(this)}
+                        onChange = {this.handleInputChange}
                         />
-                        <button onClick={this.handleBtnClick.bind(this)}>提交</button>
+                        <button onClick={this.handleBtnClick}>提交</button>
                 </div>
                 <ul>
                     {
-                        this.state.list.map(( item, index)=> {
-                            return (
-                                <div>
-                                    <TodoItem 
-                                    content={item}
-                                    index={index}
-                                    deleteItem={this.handleItemDelete.bind(this)}
-                                    />
-                                    {/*
-                                        <li
-                                        key={index}
-                                        onClick={this.hangleItemDelete.bind(this)}
-                                        >
-                                        </li>
-                                      */}
-                                </div>
-                            )
-
-                        })
+                       this.getTodoItem()
                     }
                 </ul>
                  
             </Fragment>       
  )
     }
-    handleInputChange(e){
-        this.setState({
-            inputValue:e.target.value
+    //组件即将被挂载到页面的时刻，自动被执行
+    componentWillMount(){
+        console.log('componentWillMount');
+    }
+    //组件被挂载到页面之后，自动被执行
+    componentDidMount(){
+        // axios.get('/git/React/todolist') //ajax请求
+        // .then(() =>{  alert( 'succ')})
+        // .catch(() => { alert('error')});
+        console.log('componentDidMount');
+
+    }
+    //组件被更新之前，他会自动执行
+    // shouldComponentUpdate(){
+    //     console.log('shouldComponentUpdate');
+    //     return true;
+    // }
+    //组件被更新之前执行，但是在shouldComponentUpdate之后执行，如果返回true，则执行
+    //返回false不执行。
+    componentWillUpdate(){
+        console.log('componentWillUpdate');
+    }
+    //组件更新完成之后，他会自动执行
+    componentDidUpdate(){
+        console.log('componentDidUpdate');
+    }
+    //一个组件要从父组件接收参数
+   // 只要父组件的render函数被重新执行了，子组件的这个生命周期函数就会被执行。
+    //如果这个组件第一次存在于父组件中，不会执行
+    //如果这个组件之前已经存在于父组件中，才会执行。
+    componentWillReceiveProps(){
+        console.log('componentWillReceiveProps');
+    }
+    //当这个组件即将被从页面中剔除的时候，会被执行
+    componentWillUnmount(){
+        console.log('componentWillUnmount');
+    }
+    
+    getTodoItem(){
+        return this.state.list.map(( item, index)=> {
+            return (
+                <div key={item}>
+                    <TodoItem 
+                    content={item}
+                    index={index}
+                    deleteItem={this.handleItemDelete}
+                    />
+    
+                </div>
+            )
+    
         })
+    }
+    handleInputChange(e){
+        const value=e.target.value;
+        this.setState(() => ({
+             inputValue:value
+        }))
     }
     handleBtnClick(){
-        this.setState({
-            list: [...this.state.list,this.state.inputValue],
+        this.setState((prevState) =>({
+            list: [...prevState.list,prevState.inputValue],
             inputValue:''
-        })
+        }))
     }
+    
     handleItemDelete(index){
-        const list = [...this.state.list];
-        list.splice(index,1);
-        this.setState({
-            list: list
+        this.setState((prevState)=>{
+            const list = [...prevState.list];
+            list.splice(index,1);
+            return {list}
         })
     }
     
-}
-export default TodoList;
+} 
+
+
+    export default TodoList;
